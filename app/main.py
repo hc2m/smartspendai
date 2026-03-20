af276c99df26ce18a5dea256b2b0b5ff7b68ecec
hc2m <hasanurpc233@gmail.com>
2026-03-20 21:30:58 +0530

    CORS fix

--- app/main.py
+++ app/main.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 from app.database.database import engine, Base
 from app.routes import auth_routes
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 
 app = FastAPI(title="SmartSpend AI")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.add_middleware(
     SessionMiddleware,
