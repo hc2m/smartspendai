02688a10bab969c1968d0bed19594de00f522383
hc2m <hasanurpc233@gmail.com>
2026-03-20 10:49:40 +0530

    fix session middleware

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 from app.database.database import engine, Base
-from app.routes import auth_routes, auth_google
+from app.routes import auth_routes
 from starlette.middleware.sessions import SessionMiddleware
 import os
 
+
 app = FastAPI(title="SmartSpend AI")
-app.include_router(auth_google.router)
+
 app.add_middleware(
     SessionMiddleware,
     secret_key = os.getenv("SESSION_SECRET_KEY")
 )
+
+# this is importent after imddleware add then include_router
+from app.routes import auth_google
+app.include_router(auth_google.router)
+
 
 @app.on_event("startup")
 def on_startup():
