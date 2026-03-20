677063a9d8e24b34e7dd37603bae4798ca1934b6
hc2m <hasanurpc233@gmail.com>
2026-03-20 11:56:39 +0530

    jwt connect

--- app/routes/auth_google.py
+++ app/routes/auth_google.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
+from app.routes.auth_routes import get_db
+from sqlalchemy.orm import Session
+from app.models.user import User
+from jose import jwt
 from authlib.integrations.starlette_client import OAuth
 from starlette.responses import RedirectResponse
+from app.utils.jwt_handler import create_access_token
 import os
 
 router = APIRouter()
@@ -23,8 +28,39 @@ async def login_google(request: Request):
 
 # step 2: callback from google
 @router.get("/auth/google/callback")
-async def auth_google(request: Request):
+async def auth_google(request: Request, db: Session = Depends(get_db)):
     token = await oauth.google.authorize_access_token(request)
-    user = token.get("userinfo")
+    user_info = token.get("userinfo")
 
-    return {"email": user["email"], "name": user["name"], "picture": user["picture"]}
+    email = user_info["email"]
+    name = user_info["name"]
+    picture = user_info["picture"]
+
+    # check user exists
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        # ➕ create user
+        user = User(
+            email = email,
+            username = name,
+            provider = "google",
+            picture = picture
+        )
+
+        db.add(user)
+        db.commit()
+        db.refresh()
+
+    # create 🔑 jwt using handler 
+    access_token = create_access_token({"sub":user.email})
+
+    return{
+        "access_token": access_token,
+        "token_type": "brearer",
+        "user_info":{
+            "email": user.email,
+            "name": user.username,
+            "picture":user.picture
+        }
+    }
