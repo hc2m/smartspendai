4449d072cddf9060e1e42fac4f13bb268f940e0f
hc2m <hasanurpc233@gmail.com>
2026-03-22 22:35:27 +0530

    token problem

--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -13,6 +13,8 @@ ALGORITHM = "HS256"
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
 
+    print("token = ",token)
+
     try:
         payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
         user_id = payload.get("user_id")
