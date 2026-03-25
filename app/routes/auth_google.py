63801bfa185afaa1e1dd2659957ec4ca5adccf35
hc2m <hasanurpc233@gmail.com>
2026-03-25 20:39:52 +0530

    add httpOnly cookies

--- app/routes/auth_google.py
+++ app/routes/auth_google.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request, Depends, Response
 from app.routes.auth_routes import get_db
 from sqlalchemy.orm import Session
 from app.models.user import User
@@ -6,7 +6,12 @@ from jose import jwt
 from authlib.integrations.starlette_client import OAuth
 from starlette.responses import RedirectResponse
 from app.utils.jwt_handler import create_access_token
+
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+IS_PRODUCTION = os.getenv("ENV") == "production"
 
 router = APIRouter()
 
@@ -28,7 +33,7 @@ async def login_google(request: Request):
 
 # step 2: callback from google
 @router.get("/auth/google/callback")
-async def auth_google(request: Request, db: Session = Depends(get_db)):
+async def auth_google(request: Request, response: Response, db: Session = Depends(get_db)):
     token = await oauth.google.authorize_access_token(request)
     user_info = token.get("userinfo")
 
@@ -55,9 +60,18 @@ async def auth_google(request: Request, db: Session = Depends(get_db)):
     # create 🔑 jwt using handler 
     access_token = create_access_token({"user_id":user.id, "email": user.email})
 
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        httponly=True,
+        secure=IS_PRODUCTION,
+        samesite="none" if IS_PRODUCTION else "lax",
+        max_age=3600
+    )
+
     return{
-        "access_token": access_token,
-        "token_type": "brearer",
+        # "access_token": access_token,
+        # "token_type": "brearer",
         "user_info":{
             "email": user.email,
             "name": user.username,
