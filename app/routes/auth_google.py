9408d3033f089079a06cbdee76a44a2b89077dd7
hc2m <hasanurpc233@gmail.com>
2026-04-03 08:05:37 +0530

    google auth system add redirect url

--- app/routes/auth_google.py
+++ app/routes/auth_google.py
@@ -1,17 +1,20 @@
 from fastapi import APIRouter, Request, Depends, Response
+from fastapi.responses import RedirectResponse
 from app.database.database import get_db
 from sqlalchemy.orm import Session
 from app.models.user import User
 from jose import jwt
 from authlib.integrations.starlette_client import OAuth
 from starlette.responses import RedirectResponse
-from app.utils.jwt_handler import create_access_token
+from app.utils.jwt_handler import create_access_token, create_refresh_token
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 IS_PRODUCTION = os.getenv("ENV") == "production"
+BACKEND_URL = os.getenv("BACKEND_URL")
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 
 router = APIRouter()
 
@@ -30,12 +33,12 @@ oauth.register(
 # step 1: Redirect user to google
 @router.get("/auth/google")
 async def login_google(request: Request):
-    redirect_uri = "https://api.smartspendai.org/auth/google/callback"
+    redirect_uri = f"{BACKEND_URL}/auth/google/callback"
     return await oauth.google.authorize_redirect(request, redirect_uri)
 
 # step 2: callback from google
 @router.get("/auth/google/callback")
-async def auth_google(request: Request, response: Response, db: Session = Depends(get_db)):
+async def auth_google(request: Request, db: Session = Depends(get_db)):
     token = await oauth.google.authorize_access_token(request)
     user_info = token.get("userinfo")
 
@@ -61,6 +64,9 @@ async def auth_google(request: Request, response: Response, db: Session = Depend
 
     # create 🔑 jwt using handler 
     access_token = create_access_token({"user_id":user.id, "email": user.email})
+    refresh_token = create_refresh_token({"user_id": user.id,"email": user.email})
+
+    response = RedirectResponse(url=f"{FRONTEND_URL}/")
 
     response.set_cookie(
         key="access_token",
@@ -73,12 +79,17 @@ async def auth_google(request: Request, response: Response, db: Session = Depend
         path="/"
     )
 
-    return{
-        # "access_token": access_token,
-        # "token_type": "brearer",
-        "user_info":{
-            "email": user.email,
-            "name": user.username,
-            "picture":user.picture
-        }
-    }
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        secure=IS_PRODUCTION,
+        samesite="none" if IS_PRODUCTION else "lax",
+        max_age=60 * 60 * 24 * 7,
+        domain=".smartspendai.org" if IS_PRODUCTION else None,
+        path="/"
+    )
+
+
+
+    return response
